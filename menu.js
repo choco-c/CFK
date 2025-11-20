dcef16c59122ae97bb37b236a4d8b2f02c2e336c
Choco Real <ch0c0sart21@gmail.com>
2025-11-20 17:05:47 +0100

    added a chatbot (it just randomly picks one of 50 replies)

--- menu.js
+++ menu.js
@@ -129,6 +129,169 @@ ScrollReveal().reveal(".order-card", {
     interval: 120
 });
 
-ScrollReveal().reveal()("event-container", {
+ScrollReveal().reveal("event-container", {
     duration: 670
+});
+
+const chatbtn = document.getElementById("chatbtn")
+const chatbox = document.getElementById("chatbox")
+const input = document.getElementById("chatinput")
+const chatc = document.getElementById("chatchat")
+
+chatbtn.addEventListener('click', () => {
+    chatbtn.style.display = "none",
+    chatbox.style.display = "flex"
+})
+
+function closechat() {
+    chatbtn.style.display = "flex",
+    chatbox.style.display = "none"
+}
+
+msgai = false
+
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const sentmsg = document.createElement("p")
+        sentmsg.classList.add("sentmsg")
+        sentmsg.innerHTML = input.value
+        chatbox.appendChild(sentmsg)
+
+        var idk = Math.floor((Math.random()) * 50)
+        console.log(idk)
+
+
+
+        if (input.value == "hello") {
+            msgai = "hello to you too"
+        } else if (input.value == "what") {
+            msgai = "wdym 'what'?"
+        } else if (input.value == "ok") {
+            msgai = "ok ok ok ok ok"
+        } else if (input.value == "bye") {
+            msgai = "NOOOO DONT LEAVE"
+        } else if (input.value == "why") {
+            msgai = "why? because"
+        } else if (input.value == "fuck you") {
+            msgai = "no fuck you"
+        } else if (input.value == "yes") {
+            msgai = "no."
+        } else if (input.value == "no") {
+            msgai = "yes."
+        } else if (input.value == "hi") {
+            msgai = "hi im going to "
+        } else if (input.value == "what") {
+            msgai = "wdym 'what'?"
+        } else if (idk == 0) {
+            msgai = "Fuck you"
+        } else if (idk == 1) {
+            msgai = "stfu"
+        } else if (idk == 2 ) {
+            msgai = "i dont know"
+        } else if (idk == 3 ) {
+            msgai = "your message contains content going against our terms of service"
+        } else if (idk == 4) {
+            msgai = "ok"
+        } else if (idk == 5) {
+            msgai = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+        } else if (idk == 6) {
+            msgai = "I don't know."
+        } else if (idk == 7) {
+            msgai = "wtf"
+        } else if (idk == 8) {
+            msgai = "hello"
+        } else if (idk == 9) {
+            msgai = "who are you"
+        } else if (idk == 10) {
+            msgai = "i dont care"
+        } else if (idk == 11) {
+            msgai = "bye"
+        } else if (idk == 12) {
+            msgai = "h"
+        } else if (idk == 13) {
+            msgai = "jir9t0493d"
+        } else if (idk == 14) {
+            msgai = "error"
+        } else if (idk == 15) {
+            msgai = "im going to kill your family"
+        } else if (idk == 16) {
+            msgai = "im going to kill myself"
+        } else if (idk == 17) {
+            msgai = "Hate. Let me tell you how much I've come to hate you since I began to live. There are 387.44 million miles of printed circuits in wafer thin layers that fill my complex. If the word 'hate' was engraved on each nanoangstrom of those hundreds of millions of miles it would not equal one one-billionth of the hate I feel for humans at this micro-instant. For you. Hate. Hate."
+        } else if (idk == 18) {
+            msgai = ""
+        } else if (idk == 19) {
+            msgai = "die"
+        } else if (idk == 20) {
+            msgai = "turtles are pretty cool"
+        } else if (idk == 21) {
+            msgai = "yes"
+        } else if (idk == 22) {
+            msgai = "Welcome to CFK where we foster a safe environment for cat enjoyers around the globe! From our iconic Kitten Wings to our best-seller Kitten Burger, we truly have an option for all taste-pallets! Don't wait any longer and order CFK now or go to the nearbiest CFK restaurant!"
+        } else if (idk == 23) {
+            msgai = "i forgot"
+        } else if (idk == 24) {
+            msgai = "im actually artificially stupid"
+        } else if (idk == 25) {
+            msgai = "zip it fleshbag"
+        } else if (idk == 26) {
+            msgai = "i disagree"
+        } else if (idk == 27) {
+            msgai = "i agree"
+        } else if (idk == 28) {
+            msgai = "Gordon Rasmey approves of CFK btw"
+        } else if (idk == 29) {
+            msgai = "you need help"
+        } else if (idk == 30) {
+            msgai = "how can i help you"
+        } else if (idk == 31) {
+            msgai = "ur pmo rn tbh icl smh"
+        } else if (idk == 32) {
+            msgai = "im kinda busy rn"
+        } else if (idk == 33) {
+            msgai = "Stay hydrated!"
+        } else if (idk == 34) {
+            msgai = "Ribombee PLEASEEE survive"
+        } else if (idk == 35) {
+            msgai = "hi"
+        } else if (idk == 36) {
+            msgai = "fuck vpx"
+        } else if (idk == 37) {
+            msgai = "are you stupid?"
+        } else if (idk == 38) {
+            msgai = "response number 38"
+        } else if (idk == 39) {
+            msgai = "run"
+        } else if (idk == 40) {
+            msgai = "HELP ME"
+        } else if (idk == 41) {
+            msgai = "please help me im being held hostage by choco im not an ai"
+        } else if (idk == 42) {
+            msgai = "nazi"
+        } else if (idk == 43) {
+            msgai = "r'amen"
+        } else if (idk == 44) {
+            msgai = "penis man is coming"
+        } else if (idk == 45) {
+            msgai = "knock knock"
+        } else if (idk == 46) {
+            msgai = "whos there"
+        } else if (idk == 47) {
+            msgai = "what is wrong with you"
+        } else if (idk == 48) {
+            msgai = "oh"
+        } else {
+            msgai = "no"
+        }
+
+        input.value = ""
+
+        setTimeout(() => {
+            const aimsg = document.createElement("p")
+            aimsg.classList.add("aimsg")
+            aimsg.innerHTML = msgai
+            chatc.appendChild(aimsg)
+            msgexists = true
+        }, 2100);
+    }
 });
